3be10e22916017ed57c5bec1e45430b1ed5fb803
AndresCallejas-2025141 <ecallejas-2025141@kinal.edu.gt>
2026-08-26 15:34:31 -0600

    test: Se modifico app.ts para agregar a clientes y vehiculos

--- Backend/src/app/app.ts
+++ Backend/src/app/app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import clienteRouter from '.././routes/clientesRoutes';
+import vehiculoRouter from '.././routes/vehiculosRoutes';
 import proveedoresRouter from '.././routes/proveedoresRoutes';
 
 
@@ -17,6 +19,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.use('/api/proveedores', proveedoresRouter);
+app.use('/api/clientes', clienteRouter);
+app.use('/api/vehiculos', vehiculoRouter);
 
 // Ruta de prueba
 app.get('/health', (req, res) => {
