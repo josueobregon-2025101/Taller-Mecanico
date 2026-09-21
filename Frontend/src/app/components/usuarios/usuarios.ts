5e441299f608afe76d7b92d52d8e6da2edf13227
omartobar-2025092 <otobar-2025092@kinal.edu.gt>
2026-09-21 11:02:26 -0600

    feat: Se agrego el componente para Usuarios

---
+++ Frontend/src/app/components/usuarios/usuarios.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-usuarios',
+  styleUrl: './usuarios.css',
+  templateUrl: './usuarios.html',
+})
+export class Usuarios {}
