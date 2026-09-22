165b6b91ebed4236d3ae8dd74d0904211555cc06
fernandoherrarte-2021072 <fherrarte-2021072@kinal.edu.gt>
2026-09-22 10:47:30 -0600

    feat: Se cambio la ruta principal al iniciar sesion

--- Frontend/src/app/components/login/login.ts
+++ Frontend/src/app/components/login/login.ts
@@ -81,7 +81,7 @@ export class LoginComponent {
           this.cargando = false;
 
           this.router.navigate([
-            '/clientes'
+            '/panel'
           ]);
 
           this.cdr.detectChanges();
