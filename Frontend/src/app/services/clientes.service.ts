3bcb39879e2f10c8bab7be1f503b008ae359477d
AndresCallejas-2025141 <ecallejas-2025141@kinal.edu.gt>
2026-09-22 10:32:52 -0600

    test: Se modifico este Service por el funcionamiento de CRUD

--- Frontend/src/app/services/clientes.service.ts
+++ Frontend/src/app/services/clientes.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 export interface Cliente {
-  idclientes: number;
+  idClientes: number;
   nombrecliente: string;
   apellido: string;
   documento: string;
@@ -16,6 +16,9 @@ export interface ClienteResponse {
   data: Cliente;
 }
 
+export type ClienteDatos =
+  Omit<Cliente, 'idClientes'>;
+
 @Injectable({
   providedIn: 'root'
 })
@@ -23,27 +26,58 @@ export class ClienteService {
 
   private http = inject(HttpClient);
 
-  private apiUrl = 'http://localhost:3000/api/clientes';
+  private apiUrl =
+    'http://localhost:3000/api/clientes';
 
+  // OBTENER TODOS
   obtenerClientes(): Observable<Cliente[]> {
-    return this.http.get<Cliente[]>(this.apiUrl);
+
+    return this.http.get<Cliente[]>(
+      this.apiUrl
+    );
   }
 
-  obtenerCliente(id: number): Observable<Cliente> {
-    return this.http.get<Cliente>(`${this.apiUrl}/${id}`);
+  // OBTENER POR ID
+  obtenerCliente(
+    id: number
+  ): Observable<Cliente> {
+
+    return this.http.get<Cliente>(
+      `${this.apiUrl}/${id}`
+    );
   }
 
-  crearCliente(cliente: Omit<Cliente, 'idclientes'>): Observable<ClienteResponse> {
+  // CREAR
+  crearCliente(
+    cliente: ClienteDatos
+  ): Observable<ClienteResponse> {
+
+    console.log(
+      'POST CLIENTE:',
+      cliente
+    );
+
     return this.http.post<ClienteResponse>(
       this.apiUrl,
       cliente
     );
   }
 
+  // ACTUALIZAR
   actualizarCliente(
     id: number,
-    cliente: Partial<Cliente>
+    cliente: ClienteDatos
   ): Observable<ClienteResponse> {
+
+    console.log(
+      'PUT CLIENTE:',
+      `${this.apiUrl}/${id}`
+    );
+
+    console.log(
+      'DATOS PUT:',
+      cliente
+    );
 
     return this.http.put<ClienteResponse>(
       `${this.apiUrl}/${id}`,
@@ -51,10 +85,18 @@ export class ClienteService {
     );
   }
 
-  eliminarCliente(id: number): Observable<any> {
+  // ELIMINAR
+  eliminarCliente(
+    id: number
+  ): Observable<any> {
+
+    console.log(
+      'DELETE CLIENTE:',
+      `${this.apiUrl}/${id}`
+    );
+
     return this.http.delete(
       `${this.apiUrl}/${id}`
     );
   }
-
 }
